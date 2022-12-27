f6d003607ebea91e405875270612b34b1514bbc5
Junsuk Park <park64kr63@gmail.com>
2022-12-27 21:32:32 +0900

    git init

---
+++ src/list-pagination-context-provider.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+type PaginationState = {
+  totalItems: number;
+  pageSize: number;
+  currentPage: number;
+};
+
+type PaginationMeta = {
+  totalPages: number;
+  previousEnabled: boolean;
+  nextEnabled: boolean;
+};
+
+type Pagination = PaginationState & PaginationMeta;
+
+export const usePaginationContext = create<{
+  pagination: Pagination;
+  setPagination: (pg: Pagination) => void;
+  setNextPage: () => void;
+  setFirstPage: () => void;
+}>((set, get) => ({
+  pagination: {
+    totalPages: 0,
+    pageSize: 0,
+    currentPage: 0,
+    nextEnabled: false,
+    previousEnabled: false,
+    totalItems: 0,
+  },
+  setPagination: (pagination: Pagination) => set({ pagination }),
+  setNextPage: () => {},
+  setPrevPage: () => {},
+  setFirstPage: () => {},
+}));
+
+export interface ListContextProps {
+  total: number;
+  perPage: number;
+  currentPage?: number;
+}
+
+type ListPaginationContextProps = ListContextProps;
+
+const ListPaginationContextProvider: FCC<{ value: ListPaginationContextProps }> = ({ children, value }) => {
+  const { setPagination } = usePaginationContext();
+
+  React.useEffect(() => {
+    /*
+    setPagination({
+      totalPages: totalPages,
+      pageSize: value.perPage,
+      currentPage,
+      nextEnabled,
+      previousEnabled,
+      totalItems: value.total,
+    });
+*/
+  }, [value]);
+
+  return <>{children}</>;
+};
+
+export default ListPaginationContextProvider;
